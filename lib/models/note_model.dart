0b6ec4ef321daa2c6fc2a700e169e126417d9b95
ElSanteel <alsantel.ma@gmail.com>
2024-05-06 17:22:44 +0300

    create notes view model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String title;
+  final String subTitile;
+  final String date;
+  final int color;
+
+  NoteModel({
+    required this.title,
+    required this.subTitile,
+    required this.date,
+    required this.color,
+  });
+}
