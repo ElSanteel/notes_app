0b423fcd28fb0220e70a8ef7fc70589a2ea7573d
ElSanteel <alsantel.ma@gmail.com>
2024-05-09 01:43:32 +0300

    finish note bottom sheet

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,6 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
@@ -9,14 +9,16 @@ part 'add_note_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
+  Color color = Colors.red;
   addNote(NoteModel note) async {
+    note.color = color.value;
+    
     emit(AddNoteLoading());
     try {
       // receive our box in notesBox variable
       var notesBox = Hive.box<NoteModel>(kNotesBox);
       // add our note to the box
       await notesBox.add(note);
-
       emit(AddNoteSuccess());
     } catch (e) {
       emit(AddNoteFailure(e.toString()));
