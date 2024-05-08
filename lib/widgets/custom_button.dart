c239ffbc14a19bb8ad05f2356c1baa4cdb077488
ElSanteel <alsantel.ma@gmail.com>
2024-05-08 12:32:08 +0300

    change add note bottom sheet loading

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -4,7 +4,8 @@ import '../constants.dart';
 
 class CustomButton extends StatelessWidget {
   final void Function()? onTap;
-  const CustomButton({super.key, this.onTap});
+  final bool isLoading;
+  const CustomButton({super.key, this.onTap, this.isLoading = false});
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +18,22 @@ class CustomButton extends StatelessWidget {
         ),
         width: MediaQuery.of(context).size.width,
         height: 55,
-        child: const Center(
-          child: Text(
-            'Add',
-            style: TextStyle(
-                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
-          ),
+        child: Center(
+          child: isLoading
+              ? const SizedBox(
+                  width: 30,
+                  height: 30,
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ),
+                )
+              : const Text(
+                  'Add',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
         ),
       ),
     );
